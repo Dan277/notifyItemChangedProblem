883e0a0e99d71b5ed334dd25ca470890ecc7ccdd
Dan Bray <dan.bray@gowebprint.com>
2019-03-11 10:16:24 +0000

    Initial commit

---
+++ app/src/main/java/dan277/notifyitemchangedproblem/MainActivity.kt
@@ -0,0 +1,12 @@
+package dan277.notifyitemchangedproblem
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
